ef89dffb10fbf717eb49df8b43591372626f9ee6
Passive1412 <marcusjhomann@outlook.com>
2020-09-03 16:01:23 +0200

    work on registration

--- PassiveFrontend/src/app/pages/registration/registration.component.ts
+++ PassiveFrontend/src/app/pages/registration/registration.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  busy = false;
+  _username = '';
+  _password = '';
+  _password2 = '';
+  _email = '';
+  registerError = false;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  register() {
+    console.log('register');
+    if (!this._password || !this._password2) {
+      return;
+    }
+    this.busy = true;
+    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '';
   }
-
 }
